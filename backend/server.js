97ed5963389dd471eecda4198ec825d4d764483a
Tomer Negad <tomernegad@gmail.com>
2025-05-15 10:36:33 +0300

    feat: configure server with environment variables and initial setup

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,24 @@
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
 
-
+dotenv.config();
 
 const app = express();
-app.use(helmet());
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());// secure middlware that help protect my app by setting varios HTTP headers.
+app.use(morgan("dev")); // log the requests
 
 app.get("/",(req,res)=>{
-    console.log(res.getHeaders());
     res.send("hello from backend");
 })
 
-app.listen(3000,()=>{
-    console.log("The server is run on port 3000");
+app.listen(PORT,()=>{
+    console.log(`The server is run on port ${PORT}`);
 }
 )
