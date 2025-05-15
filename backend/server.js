1e2f97abb6ff2a54ffe87de414581b449cd15dfa
Tomer Negad <tomernegad@gmail.com>
2025-05-15 11:08:30 +0300

    feat: update server configuration and add product routes with controllers

--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,19 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
 app.use(helmet());// secure middlware that help protect my app by setting varios HTTP headers.
 app.use(morgan("dev")); // log the requests
 
-app.get("/",(req,res)=>{
-    res.send("hello from backend");
-})
+app.use("/api/products",productRoutes)
 
 app.listen(PORT,()=>{
     console.log(`The server is run on port ${PORT}`);
