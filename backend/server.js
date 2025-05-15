3982633041902026551031c26cabf10153a4e070
Tomer Negad <tomernegad@gmail.com>
2025-05-15 13:03:01 +0300

    fix: update database password in .env and refactor server.js to initialize database

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
+import {sql} from "./config/db.js"
 
 dotenv.config();
 
@@ -17,7 +18,25 @@ app.use(morgan("dev")); // log the requests
 
 app.use("/api/products",productRoutes)
 
-app.listen(PORT,()=>{
+async function initDB(){
+    try{
+        await sql`
+            CREATE TABLE IF NOT EXISTS products(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            image VARCHAR(255) NOT NULL,
+            price DECIMAL(10,2) NOT NULL,
+            create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        `;
+    }
+    catch(error){
+        console.log("Error initDB",error);
+    }
+}
+
+initDB().then(app.listen(PORT,()=>{
     console.log(`The server is run on port ${PORT}`);
 }
-)
+));
