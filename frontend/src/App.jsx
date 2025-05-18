9df5fe59ccadf2ec2ca318050752b1a6a6403dd2
Tomer Negad <tomernegad@gmail.com>
2025-05-18 11:32:36 +0300

    feat: add ProductPage route and wrap App component with BrowserRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import Navbar from "./components/Navbar";
-import {Routes,Route} from "react-router-dom";
+
 import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+
+import {Routes,Route} from "react-router-dom";
+
 function App() {
   return (
    <div className="min-h-screen bg-base-200 transition-colors duaration-300">
       <Navbar/>
       <Routes>
         <Route path="/" element = {<HomePage/>} />
+         <Route path="/product/:id" element = {<ProductPage/>} />
       </Routes>
      </div>
   )
