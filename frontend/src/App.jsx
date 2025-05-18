a70f918309a5fb042d342390d3a6fb17e88e9ca6
Tomer Negad <tomernegad@gmail.com>
2025-05-18 14:02:04 +0300

    feat: implement theme selection and product management features; add ThemeSelector component, update Navbar and App components, and create useProductStore for product CRUD operations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
+import themes from "daisyui/theme/object";
 import Navbar from "./components/Navbar";
 
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
+import { useThemeStore } from "./store/useThemeStore";
 
 import { Routes, Route } from "react-router-dom";
 
 function App() {
+    const { theme } = useThemeStore();
   return (
-    <div className="min-h-screen bg-base-200 transition-colors duration-200" data-theme ="forest">
+    <div className="min-h-screen bg-base-200 transition-colors duration-200" data-theme ={theme}>
       <Navbar />
 
       <Routes>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,14 @@
-import { Link} from "react-router-dom";
-import { ShoppingCartIcon } from "lucide-react";
+import { Link, useResolvedPath } from "react-router-dom";
+import { ShoppingBagIcon, ShoppingCartIcon } from "lucide-react";
+import ThemeSelector from "./ThemeSelector";
+import { useProductStore } from "../store/useProductStore.js";
 
 function Navbar() {
-  
+  const { pathname } = useResolvedPath();
+  const isHomePage = pathname === "/";
+
+  const { products } = useProductStore();
+
   return (
     <div className="bg-base-100/80 backdrop-blur-lg border-b border-base-content/10 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto">
@@ -22,6 +28,21 @@ function Navbar() {
             </Link>
           </div>
 
+          {/* RIGHT SECTION */}
+          <div className="flex items-center gap-4">
+            <ThemeSelector />
+
+            {isHomePage && (
+              <div className="indicator">
+                <div className="p-2 rounded-full hover:bg-base-200 transition-colors">
+                  <ShoppingBagIcon className="size-5" />
+                  <span className="badge badge-sm badge-primary indicator-item">
+                    {products.length}
+                  </span>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
